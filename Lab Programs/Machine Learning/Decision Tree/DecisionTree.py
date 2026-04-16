3f1af4d3c80d89a477c781dff4880ccc63cf34d2
etherealsoup <studiousferoz@gmail.com>
2026-04-16 21:01:27 +0530

    Update DecisionTree.py

--- Lab Programs/Machine Learning/Decision Tree/DecisionTree.py
+++ Lab Programs/Machine Learning/Decision Tree/DecisionTree.py
@@ -2,12 +2,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
-data = pd.read_csv("diabetes.csv")
-X = data.drop("Outcome", axis=1)
-y = data["Outcome"]
+data = pd.read_csv("data.csv")
+print(data.head())
+X = data.drop("target", axis=1)
+y = data["target"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 model = DecisionTreeClassifier()
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy:", accuracy)
+print("Accuracy:", accuracy_score(y_test, y_pred))
